d055ba39ca7d5bab6b1748d452f202a053b3607a
Cristian Arevalo <crsarevalo@gmail.com>
2024-01-19 09:24:31 -0300

    agregando log

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import joyasRoutes from "./Routes/joyasRoutes.js";
-
+import { logger } from "logger-express";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(logger());
 app.use("/api", joyasRoutes);
 
 app.listen(PORT, () => {
